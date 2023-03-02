1e8690beaebbcad1bb7d2b8771d89ec48b92e8c7
Floriane Davadie <floriane.davadie@laposte.net>
2023-03-02 18:48:02 +0100

    profile photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_112002) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_170422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_112002) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
