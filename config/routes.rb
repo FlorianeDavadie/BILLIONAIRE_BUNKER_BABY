c8f5480de43d44763479d8205716d14e06d4a54b
Massilia Bakhouche <massiliabakhouche@gmail.com>
2023-03-03 10:19:34 +0100

    booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :bunkers do
-    resources :bookings, only: %i[new show create edit update destroy]
+    resources :bookings, only: %i[new create edit update destroy]
   end
+
+  resources :bookings, only: :show
   # get "/bunkers/:id", to: "bunkers#show_bunkers"
   get "/profil", to: "pages#profil"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_112002) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_170129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,13 +43,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_112002) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.text "date_debut"
-    t.text "date_fin"
     t.float "price"
     t.bigint "user_id", null: false
     t.bigint "bunker_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["bunker_id"], name: "index_bookings_on_bunker_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
