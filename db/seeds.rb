fe1c2d655974666240be23d660cd47b92288ce38
Floriane Davadie <floriane.davadie@laposte.net>
2023-03-03 12:32:35 +0100

    seed modifs

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,9 @@ user1.photo.attach(io: user1_file, filename: "user1.jpg", content_type: "image/j
 user1.save!
 
 file = URI.open("https://res.cloudinary.com/dz19chcgd/image/upload/v1677600895/nandor-muzsik-2T47CztPSyM-unsplash_bhexpd.jpg")
-bunker = Bunker.new(title: "Diamond Bunker", description: "lorem12", prix: 2000, user: user1)
+bunker = Bunker.new(title: "Diamond Bunker", description: "Pretoria - South Africa", location: "Welcome to Diamond Bunker! Elegant eastside and recently renovated Bunker! We are a secluded getaway, perfect for privacy. This is an ideal rental for couples or families.
+  Wifi internet, and cable TV, and every options are available.
+  Access to a pool area, which includes a kiddie pool and hot tub. Access to a gym, basketball court, tennis court, and playground.", prix: 2000, user: user1)
 bunker.photo.attach(io: file, filename: "bunker1.jpg", content_type: "image/jpg")
 bunker.save!
 
@@ -32,7 +34,8 @@ user2.photo.attach(io: user2_file, filename: "user2.png", content_type: "image/p
 user2.save!
 
 file1 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677672147/alex-wong-l5Tzv1alcps-unsplash_d0gt0a.jpg")
-bunker = Bunker.new(title: "Gold Bunker", description: "lorem11", prix: 4000, user: user2)
+bunker = Bunker.new(title: "Gold Bunker", description: "Roubaix - France", location: "Welcome to Gold Bunker! Quiet, safe, and gated, this paradise will take your breath away!It was featured on magazine & reality tv show. The property is over 10,000 sq ft. with 6 bedrooms & 7 bathrooms.
+  Each bedroom has its private bathroom for convenience. Also, a stunning Gourmet Kitchens & dining room.Great space for celebrities to stay during tours or just to relax.", prix: 4000, user: user2)
 bunker.photo.attach(io: file1, filename: "bunker2.png", content_type: "image/png")
 bunker.save!
 
@@ -42,7 +45,7 @@ user3.photo.attach(io: user3_file, filename: "user3.jpg", content_type: "image/j
 user3.save!
 
 file2 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677676258/rod-long-2P_ifaetDm0-unsplash_oghyic.jpg")
-bunker = Bunker.new(title: "Green Bunker", description: "lorem13", prix: 10_000, user: user3)
+bunker = Bunker.new(title: "Green Bunker", description: "Melbourne - Australia", location: "", prix: 10_000, user: user3)
 bunker.photo.attach(io: file2, filename: "bunker3.jpg", content_type: "image/jpg")
 bunker.save!
 
@@ -52,7 +55,7 @@ user4.photo.attach(io: user4_file, filename: "user4.jpg", content_type: "image/j
 user4.save!
 
 file3 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677672145/don-shetterly-VYZKzvk7qTw-unsplash_vyg8ob.jpg")
-bunker = Bunker.new(title: "Blue Bunker", description: "lorem14", prix: 10_000, user: user4)
+bunker = Bunker.new(title: "Blue Bunker", description: "Seattle - USA", location: "", prix: 10_000, user: user4)
 bunker.photo.attach(io: file3, filename: "bunker4.jpg", content_type: "image/jpg")
 bunker.save!
 
@@ -62,7 +65,7 @@ user5.photo.attach(io: user5_file, filename: "user5.jpg", content_type: "image/j
 user5.save!
 
 file4 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677676259/ralph-ravi-kayden-zSG-kd-L6vw-unsplash_jopgpr.jpg")
-bunker = Bunker.new(title: "Black Bunker", description: "lorem15", prix: 20_000, user: user5)
+bunker = Bunker.new(title: "Black Bunker", description: "Honolulu - Hawaï", location: "", prix: 20_000, user: user5)
 bunker.photo.attach(io: file4, filename: "bunker5.jpg", content_type: "image/jpg")
 bunker.save!
 
@@ -72,7 +75,7 @@ user6.photo.attach(io: user6_file, filename: "user6.jpg", content_type: "image/j
 user6.save!
 
 file5 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677680893/mark-champs-Id2IIl1jOB0-unsplash_ixh2ew.jpg")
-bunker = Bunker.new(title: "Yellow Bunker", description: "lorem16", prix: 15_000, user: user6)
+bunker = Bunker.new(title: "Yellow Bunker", description: "Wellington - New-Zealand", location: "", prix: 15_000, user: user6)
 bunker.photo.attach(io: file5, filename: "bunker6.jpg", content_type: "image/jpg")
 bunker.save!
 
@@ -82,7 +85,7 @@ user7.photo.attach(io: user7_file, filename: "user7.jpg", content_type: "image/j
 user7.save!
 
 file6 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677680893/huum-NHLS5hOSH0c-unsplash_g8htso.jpg")
-bunker = Bunker.new(title: "Grey Bunker", description: "lorem17", prix: 25_000, user: user7)
+bunker = Bunker.new(title: "Grey Bunker", description: "Bab El Oued - Algeria", location: "", prix: 25_000, user: user7)
 bunker.photo.attach(io: file6, filename: "bunker7.jpg", content_type: "image/jpg")
 bunker.save!
 
@@ -92,7 +95,7 @@ user8.photo.attach(io: user8_file, filename: "user8.jpg", content_type: "image/j
 user8.save!
 
 file7 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677672145/alex-holzreiter-i7nqRB8Fd0Y-unsplash_om7uof.jpg")
-bunker = Bunker.new(title: "Platinium Bunker", description: "lorem18", prix: 8_000, user: user8)
+bunker = Bunker.new(title: "Platinium Bunker", description: "São Paulo - Brazil", location: "", prix: 8_000, user: user8)
 bunker.photo.attach(io: file7, filename: "bunker8.jpg", content_type: "image/jpg")
 bunker.save!
 
@@ -102,7 +105,7 @@ user9.photo.attach(io: user9_file, filename: "user9.jpg", content_type: "image/j
 user9.save!
 
 file8 = URI.open("https://res.cloudinary.com/dzwtudz1z/image/upload/v1677681385/adam-winger-To4Yip8rc-Q-unsplash_smnl03.jpg")
-bunker = Bunker.new(title: "Ruby Bunker", description: "lorem19", prix: 69_000, user: user9)
+bunker = Bunker.new(title: "Ruby Bunker", description: "Punta Cana - Dominican Republic", location: "", prix: 69_000, user: user9)
 bunker.photo.attach(io: file8, filename: "bunker9.jpg", content_type: "image/jpg")
 bunker.save!
 
